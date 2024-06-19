a54ba7bfa21586bbc2334d4e5805249e711f9e9f
Swapnilpatil03 <155384505+Swapnilpatil03@users.noreply.github.com>
2024-06-19 18:47:19 +0530

    Update JoinTestcode.java

--- JoinTestcode.java
+++ JoinTestcode.java
@@ -127,10 +127,8 @@ public class JoinTestcode extends JFrame {
                 } else {
                     JOptionPane.showMessageDialog(null, "Join the Test of code: " + tx1.getText(), "Info", JOptionPane.INFORMATION_MESSAGE);
                     ex = fetchip(tx1.getText());
-//                    Clientconnection cn = new Clientconnection();
-//                    cn.conToExaminer(ex);
                     setVisible(false);
-                    new NewJFrame().setVisible(true);
+                    new Profile().setVisible(true);
                     
                 }
                 
@@ -139,39 +137,7 @@ public class JoinTestcode extends JFrame {
         });
     
 }
-    public void connectToExaminer(String ex){
-        
-        if (isConnected == false) 
-        {
-           
-
-            try 
-            {
-                address = ex;
-                sock = new Socket(address, port);
-                InputStreamReader streamreader = new InputStreamReader(sock.getInputStream());
-                reader = new BufferedReader(streamreader);
-                writer = new PrintWriter(sock.getOutputStream());
-//                writer.println(username + ":has connected.:Connect");
-//                writer.flush(); 
-//                ta_chat.append("You are Connected to Server.\n");
-                showConfirmDialog(null,"You are Connected to Server. !");
-                isConnected = true; 
-            } 
-            catch (Exception exx) 
-            {
-//                ta_chat.append("Cannot Connect! Try Again. \n");
-               // tf_username.setEditable(true);
-            }
-            
-           
-            
-        } else if (isConnected == true) 
-        {
-//            ta_chat.append("You are already connected. \n");
-        }
-        
-    }
+    
 
     
    public String fetchip(String examcode) {
